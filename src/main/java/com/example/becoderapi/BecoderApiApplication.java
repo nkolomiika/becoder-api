c85789a76f054c573d1b8c087d58caf6bdd97501
Nokitka <72339455+Nokitka@users.noreply.github.com>
2024-04-16 02:25:00 +0300

    init

---
+++ src/main/java/com/example/becoderapi/BecoderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.becoderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecoderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BecoderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/becoderapi/BecoderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.becoderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecoderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

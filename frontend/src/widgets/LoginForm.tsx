f5d434c7ac8068dd571dc20389b98e48e75cd548
ITerNik <ternik2705@list.ru>
2024-06-18 18:33:43 +0300

    Added login functional testing

--- frontend/src/widgets/LoginForm.tsx
+++ frontend/src/widgets/LoginForm.tsx
@@ -10,6 +10,7 @@ interface FormValues {
 export function LoginForm() {
     const [responseBody, setResponseBody] = useState<FormValues>({username: "", password: ""})
     const navigate = useNavigate()
+    const [errors, setErrors] = useState<string[]>([])
     const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         const {name, value} = event.target
         setResponseBody({...responseBody, [name]: value})
@@ -20,18 +21,21 @@ export function LoginForm() {
         axios.post('http://localhost:6868/api/auth/login', {
             login: responseBody.username,
             password: responseBody.password
-        }).then(res => {sessionStorage.setItem("token", res.data); console.log(res.data); navigate('/')})
-            .catch(err => alert(err))
+        }).then(res => {sessionStorage.setItem("token", res.data.token); console.log(res.data); navigate('/')})
+            .catch(err => setErrors([...errors, err.message]))
     }
 
     return (
         <form className="flex flex-col gap-2 mt-28" onSubmit={onSubmitHandler}>
+            <div id="error-message" title="error-message"
+                 className="text-center text-red-500">{errors.length > 0 && errors[0]}</div>
             <h2 className="text-3xl text-center font-bold">Log in</h2>
             <Input title='Username' name="username" onChange={inputChangeHandler}/>
             <Input title='Password' name="password" onChange={inputChangeHandler}/>
             <button className="bg-blue-400 rounded-xl text-white p-4 text-xl font-bold curson-pointer">Log in</button>
             <p className="text-xl">Don't have an account yet? <NavLink to="/Register"
-                                                                       className="underline text-orange-500">Go register</NavLink></p>
+                                                                       className="underline text-orange-500">Go
+                register</NavLink></p>
         </form>
     )
 }
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,87 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginTest {
+    private static WebDriver driver;
+    private static final String HOST = "http://localhost:5173/";
+
+    @BeforeAll
+    public static void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Chrome Driver\\chromedriver.exe");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void testLoginSuccess() {
+        driver.get(HOST + "login");
+
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        WebElement registerButton = driver.findElement(By.cssSelector("button"));
+
+        usernameInput.sendKeys("testuser");
+        passwordInput.sendKeys("password123");
+
+        registerButton.click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String token = (String) ((JavascriptExecutor) driver).executeScript("return sessionStorage.getItem('token');");
+        assertNotNull(token, "Token should be set in sessionStorage");
+
+        assertEquals(HOST, driver.getCurrentUrl(), "Should redirect to welcome page");
+}
+
+    @Test
+    public void testLoginFailed() {
+        driver.get(HOST + "login");
+
+        ((JavascriptExecutor) driver).executeScript("sessionStorage.removeItem('token');");
+
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        WebElement registerButton = driver.findElement(By.cssSelector("button"));
+
+        usernameInput.sendKeys("testuser");
+        passwordInput.sendKeys("invalid-password");
+
+        registerButton.click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertDoesNotThrow(() -> {
+            WebElement errorMessage = driver.findElement(By.id("error-message"));
+
+            String token = (String) ((JavascriptExecutor) driver).executeScript("return sessionStorage.getItem('token');");
+
+            assertNull(token, "Token shouldn't be set in sessionStorage");
+            assertEquals(HOST + "login", driver.getCurrentUrl(), "Should stay at login page");
+        }, "Error message should be shown");
+
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
